f3de5001b539fade4d5e3ca92b54d719c2f4b477
Imisi Bailey <imisibailey@gmail.com>
2025-11-05 14:10:30 +0100

    Created a riddle and joke creator

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.querySelector('.button')
       const riddle = data.riddle
       const answer = data.answer
       document.querySelector('.container')
-        .innerHTML += `<button class="answer button">answer</button>`
+        .innerHTML += `<button class="answer">answer</button>`
       setTimeout(() => {
 
       document.querySelector('.riddle')
@@ -15,8 +15,29 @@ document.querySelector('.button')
       }, 1000)
       document.querySelector('.answer')
         .addEventListener('click', () => {
-          document.querySelector('.container').innerHTML += `<p>${answer}</p> <p class="refresh">Refresh page for another riddle</p>`
+          document.querySelector('.container').innerHTML += `<p>${answer}</p>`
         })
+      document.querySelector('.refresh').innerText = 'Refresh page for another riddle'
     }
     getAPI()
   })
+
+  document.querySelector('.joke-button')
+    .addEventListener('click', () => {
+      const jokeLink = "https://official-joke-api.appspot.com/jokes/programming/random"
+      const getAPI = async() => {
+        const response = await fetch(jokeLink)
+        const data = await response.json()
+        const setup = data[0].setup
+        const punchline = data[0].punchline
+        console.log(punchline);
+        console.log(setup)
+
+        document.querySelector('.setup').innerHTML = setup
+        setTimeout(() => {
+          document.querySelector('.punchline').innerHTML = punchline
+        }, 2000)
+      document.querySelector('.refresh').innerText = 'Refresh page for another joke'
+      }
+      getAPI()
+    })
